a1553c07f019cccef6b3cc7866de645c02b3a74b
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2026-05-04 09:46:10 +0300

    переделал под новую отправку TLE

---
+++ website/static/js/features/api/postSatellites.js
@@ -0,0 +1,28 @@
+/**
+ * Функция отправки данных КА на сервер.
+ *
+ * @param {Object} data - тело запроса со всеми данными.
+ * @param {String} url - url адрес сервера.
+ * @param {String} group - группировка КА
+ * @param {Number} country_id - id страны КА
+ * @param {Number} purpose_id - id назначения КА
+ * @param {Number} organization_id - id организации КА
+ *
+ *
+ * */
+export async function postSatellites(data,url,{group,country_id,purpose_id,organization_id  }){
+    try {
+        const response = await fetch(`http://${url}//satellites/from_bc?group=${group}&country_id=${Number(country_id)}&purpose_id=${Number(purpose_id)}&organization_id=${Number(organization_id)}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        return result;
+    }
+    catch (error) {
+        console.error("Error add row:", error);
+    }
+}
--- website/static/js/features/flows/eventSendFlow.js
+++ website/static/js/features/flows/eventSendFlow.js
@@ -2,6 +2,7 @@ import {TleParser} from "../parsers/TleParser.js";
 import {renderPopup} from "../../components/Popup.js";
 import {LogView} from "../../components/LogView.js";
 import {postKA} from "../api/postKa.js";
+import {postSatellites} from "../api/postSatellites.js"
 import {recalculateKas} from "../api/recalculateKas.js";
 /**
  * Функция отправляет распаршенные данные по всем КА и после отправки пересчитывает по этим данным значения на сервере.
@@ -25,17 +26,43 @@ export function eventSend(){
         dataTle.NAIM_RUS = dataTle.NAIM_RUS.replace(/\n/g, "")
         dataTle.TLE_NAME = dataTle.TLE_NAME.replace(/\n/g, "")
     })
-    postKA(arr,selectedValue)
-        .then(()=>{
-            console.log(idElem)
-            LogView.showBegLogRequest(arr,'#comtainer-logs');
-            renderPopup(document.querySelector('#dialog-res'),`Данные  отправлены <br> Ожидайте сообщения о пересчете местоположения КА`)
-            if(document.getElementById('clear').checked){
-                recalculateKas(selectedValue).then((res)=>{
-                    LogView.showEndLogRequest('#comtainer-logs');
-                })
-            }
+    const newArrTle=[]
+    const  selectedValues = document.querySelector('input[name="type_group"]:checked').value;
+    const group = selectedValues === '1'?document.getElementById('groups_TLE').value ?? '':document.getElementById('group_TLE').value ?? '';
 
-        })
+    const paramRequest={
+        group,
+        country_id:Number(document.getElementById('country').value ?? ''),
+        purpose_id:Number(document.getElementById('type').value ?? ''),
+        organization_id:Number(document.getElementById('organiztion').value ?? '')
+    }
+    console.log('paramsRequest',paramRequest);
+    arr.forEach(dataTle=>{
+        const newBodyTle={}
+        newBodyTle.name=dataTle.NAIM;
+        newBodyTle.norad_id=dataTle.KOD_NORAD;
+        newBodyTle.tle_created_at=dataTle.DATA_BEG;
+        newBodyTle.tle_line_1=dataTle.TLE_LINE1;
+        newBodyTle.tle_line_2=dataTle.TLE_LINE2;
+        newArrTle.push(newBodyTle)
+    })
+    console.log('newArrTle',newArrTle);
+    postSatellites(newArrTle,selectedValue,paramRequest).then((res)=>{
+        LogView.showBegLogRequest(arr,'#comtainer-logs');
+        renderPopup(document.querySelector('#dialog-res'),`Данные  отправлены <br> Ожидайте сообщения о пересчете местоположения КА`)
+        LogView.showEndLogRequest('#comtainer-logs');
+    })
+    // postKA(arr,selectedValue)
+    //     .then(()=>{
+    //         console.log(idElem)
+    //         LogView.showBegLogRequest(arr,'#comtainer-logs');
+    //         renderPopup(document.querySelector('#dialog-res'),`Данные  отправлены <br> Ожидайте сообщения о пересчете местоположения КА`)
+    //         if(document.getElementById('clear').checked){
+    //             recalculateKas(selectedValue).then((res)=>{
+    //                 LogView.showEndLogRequest('#comtainer-logs');
+    //             })
+    //         }
+    //
+    //     })
     document.getElementById('task-btn-TLE').disabled=true;
 }
